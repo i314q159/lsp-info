9dc4eb461a379043dc8780ebd4c1fabbc8a3c3c1
i314q159 <i314q159@outlook.com>
2023-11-16 09:10:49 +0800

    update

--- lua/lsp-info.lua
+++ lua/lsp-info.lua
@@ -4,9 +4,11 @@ local config = {}
 
 function M.lsp_info()
 	local names = {}
+
 	for _, server in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
 		table.insert(names, server.name)
 	end
+
 	if next(names) ~= nil then
 		return table.concat(names, "/")
 	else
