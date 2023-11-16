0bbe53d49363dbca4c7e869e9cc717d27557ef3c
i314q159 <i314q159@outlook.com>
2023-11-16 08:56:52 +0800

    update

--- lua/lsp-info.lua
+++ lua/lsp-info.lua
@@ -1,18 +1,21 @@
 local M = {}
 
-local config = {
-}
+local config = {}
 
 function M.lsp_info()
-    local names = {}
-    for _, server in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
-        table.insert(names, server.name)
-    end
-    return "󰙴 " .. table.concat(names, "/")
+	local names = {}
+	for _, server in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+		table.insert(names, server.name)
+	end
+	if next(names) ~= nil then
+		return table.concat(names, "/")
+	else
+		return ""
+	end
 end
 
 function M.setup(opts)
-  config = vim.tbl_deep_extend("force", config, opts or {})
+	config = vim.tbl_deep_extend("force", config, opts or {})
 end
 
 return M
